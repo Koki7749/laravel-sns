e4d45525ceecc453b561ca4739f95fd11a7864b0
Koki7749 <ito8864@gmail.com>
2020-06-07 20:50:49 +0900

    テスト失敗時のmerge保護を検証2

--- tests/Feature/ArticleControllerTest.php
+++ tests/Feature/ArticleControllerTest.php
@@ -14,7 +14,7 @@ class articleControllerTest extends TestCase
     public function testIndex()
     {
         $response = $this->get(route('articles.index'));
-        $response->assertStatus(400)
+        $response->assertStatus(200)
             ->assertViewIs('articles.index');
     }
 
