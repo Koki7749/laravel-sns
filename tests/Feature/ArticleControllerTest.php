60b647219a02ca45f65971acd26d6f7e02c8d3b0
Koki7749 <ito8864@gmail.com>
2020-06-07 18:05:58 +0900

    unitテストのログイン機能

--- tests/Feature/ArticleControllerTest.php
+++ tests/Feature/ArticleControllerTest.php
@@ -2,17 +2,35 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class aplicationTest extends TestCase
+class articleControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testIndex(){
+    public function testIndex()
+    {
         $response = $this->get(route('articles.index'));
         $response->assertStatus(200)
             ->assertViewIs('articles.index');
     }
+
+    public function testGuestCreate()
+    {
+        $response = $this->get(route('articles.create'));
+        $response->assertRedirect(route('login'));
+    }
+
+    public function testAuthCreate()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)
+            ->get(route('articles.create'));
+        $response->assertStatus(200)
+            ->assertViewIs('articles.create');
+    }
+
 }
